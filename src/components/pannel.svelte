3d2f353c4cc0866d9330a2a883aa113ae4dea4a7
soham sarkar <sohamsarkar59@gmail.com>
2024-01-09 01:09:36 +0530

    intialized project and made side bar and pannel

---
+++ src/components/pannel.svelte
@@ -0,0 +1,37 @@
+<script>
+    import { IconList, IconCalendar, IconHourglassHigh, IconDeviceAnalytics, IconSettings, IconUser } from '@tabler/icons-svelte';
+    export let title;
+    export let pannelNo;
+
+    let icon;
+    $: {
+        switch (pannelNo) {
+            case 1:
+                icon = IconList;
+                break;
+            case 2:
+                icon = IconCalendar;
+                break;
+            case 3:
+                icon = IconHourglassHigh;
+                break;
+            case 4:
+                icon = IconDeviceAnalytics;
+                break;
+            case 5:
+                icon = IconSettings;
+                break;
+            case 6:
+                icon = IconUser;
+                break;
+            default:
+                icon = IconList;
+        }
+    }
+</script>
+<div class="variant-glass-surface rounded-r-lg w-96 overflow-hidden">
+    <div class="variant-glass-surface h-8 flex p-1">
+        <span><svelte:component this={icon} /></span>
+        <span class="font-bold ml-1">{title}</span>
+    </div>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+	import '../app.postcss';
+	import { AppShell } from '@skeletonlabs/skeleton';
+	import Pannel from '../components/pannel.svelte';
+	import {
+		IconList,
+		IconCalendar,
+		IconHourglassHigh,
+		IconDeviceAnalytics,
+		IconSettings,
+		IconUser
+	} from '@tabler/icons-svelte';
+</script>
+
+<AppShell slotSidebarLeft="p-4 flex w-2/5">
+	<svelte:fragment slot="sidebarLeft">
+		<div class="variant-glass-surface h-full w-14 rounded flex flex-col justify-between">
+			<div class="flex flex-col gap-2">
+				<span class="flex justify-center p-1 mt-2">
+					<IconList size={40} />
+				</span>
+				<span class="flex justify-center p-1 m-2">
+					<IconCalendar size={40} />
+				</span>
+				<span class="flex justify-center p-1 m-2">
+					<IconHourglassHigh size={40} />
+				</span>
+				<span class="flex justify-center p-1 m-2">
+					<IconDeviceAnalytics size={40} />
+				</span>
+			</div>
+			<div>
+				<span class="flex justify-center p-1 m-2">
+					<IconSettings size={40} />
+				</span>
+				<span class="flex justify-center p-1 m-2">
+					<IconUser size={40} />
+				</span>
+			</div>
+		</div>
+		<Pannel title="Hey I'm Working!!" pannelNo={1}/>
+	</svelte:fragment>
+	<slot />
+	<svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
+</AppShell>
