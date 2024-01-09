ddbe96ac6222c412fd27767e39c619ba88478daa
soham sarkar <sohamsarkar59@gmail.com>
2024-01-10 00:53:25 +0530

    made quote component

---
+++ src/components/qoute.svelte
@@ -0,0 +1,8 @@
+<span
+	class="variant-glass-surface h-fit w-1/4 rounded-md p-5 left-2/4 bottom-4 absolute -translate-x-1/2"
+>
+	<p class="text-2xl text-center h-2/3">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, dolorum.
+	</p>
+    <p class=" text-lg text-center">author</p>
+</span>
