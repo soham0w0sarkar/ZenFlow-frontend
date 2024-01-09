08ef0fe54005209a3d844d84f861576412ac7da7
soham sarkar <sohamsarkar59@gmail.com>
2024-01-10 00:55:06 +0530

    made widget component

---
+++ src/components/widgets.svelte
@@ -0,0 +1,8 @@
+<div class="variant-glass-surface rounded-md h-1/2 w-60 p-3">
+    <span class="variant-filled-primary h-full w-1/2">
+        weshd
+    </span>
+    <span>
+        date
+    </span>
+</div>
