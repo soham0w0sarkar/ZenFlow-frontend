a0a8de11353afaf44911e2b194a80fcd1362d0ea
soham sarkar <sohamsarkar59@gmail.com>
2024-01-10 23:42:09 +0530

    adding formatting

--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -8,7 +8,7 @@
 <dialog
 	bind:this={dialog}
 	on:close={() => (showModal = false)}
-	class="variant-ghost-surface flex p-2 justify-center items-center rounded-md h-1/4 w-1/4 {showModal
+    class="variant-ghost-surface flex p-2 justify-center items-center rounded-md h-1/4 w-1/4 {showModal
 		? 'block'
 		: 'hidden'}"
 >
