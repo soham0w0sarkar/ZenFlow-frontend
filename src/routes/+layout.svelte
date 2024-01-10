adc71d03545bd35bd75e004536168ad576ee91be
soham sarkar <sohamsarkar59@gmail.com>
2024-01-10 23:38:52 +0530

    adding backgound upload

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
 	import SideBar from '../components/sideBar.svelte';
 	import Qoute from '../components/qoute.svelte';
 	import Widgets from '../components/widgets.svelte';
+	import BackgroundUp from '../components/backgroundUp.svelte';
 
 	let selected = true;
 	let pannelNo = 1;
@@ -17,7 +18,11 @@
 	};
 </script>
 
-<AppShell slotSidebarLeft="p-4 flex w-fit" slotPageFooter="static h-1/4 p-4" slotPageHeader="h-1/5 p-4 flex justify-end">
+<AppShell
+	slotSidebarLeft="p-4 flex w-fit"
+	slotPageFooter="static h-1/4 p-4"
+	slotPageHeader="h-1/5 p-4 flex justify-end"
+>
 	<svelte:fragment slot="sidebarLeft">
 		<SideBar {selected} {pannelNo} on:clicked={(e) => handleClick(e.detail)} />
 		<Pannel {title} {pannelNo} {selected} />
@@ -26,5 +31,5 @@
 		<Widgets />
 	</svelte:fragment>
 	<slot />
-	<svelte:fragment slot="pageFooter"><Qoute /></svelte:fragment>
+	<svelte:fragment slot="pageFooter"><Qoute /><BackgroundUp /></svelte:fragment>
 </AppShell>
