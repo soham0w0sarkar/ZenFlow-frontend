a4b20625e0ad33d6bb018ca81ca92ec37f60c5ed
soham sarkar <sohamsarkar59@gmail.com>
2024-01-12 21:47:53 +0530

    added functionality to google login button

--- src/components/googleBtn.svelte
+++ src/components/googleBtn.svelte
@@ -1,6 +1,20 @@
-<button class="variant-filled-primary p-1 rounded-sm w-2/5 flex items-center h-10">
-    <span class="h-full w-1/4 rounded-sm bg-white mr-5 flex justify-center items-center">
-        <img src="https://img.icons8.com/color/48/000000/google-logo.png" class="h-full" alt="google ka logo hona chahiye!!"/>
-    </span>
-    <span class="text-xl">Sign in</span>
-</button>
+<script>
+    const handleClick = async() => {
+        try {
+            window.location.href = ('http://localhost:9090/auth/google');   
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    export let data;
+</script>
+
+{#if data && data.data}
+    <button class="variant-filled-primary p-1 rounded-sm w-2/5 flex items-center h-10" on:click={handleClick} >
+        <span class="h-full w-1/4 rounded-sm bg-white mr-5 flex justify-center items-center">
+            <img src="https://img.icons8.com/color/48/000000/google-logo.png" class="h-full" alt="google ka logo hona chahiye!!"/>
+        </span>
+        <span class="text-2xl text-white">Sign in</span>
+    </button>
+{/if}
