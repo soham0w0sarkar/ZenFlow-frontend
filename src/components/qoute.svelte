b421041e5efd3ff0f4c2d9cf99dd6ed8308b9c67
soham sarkar <sohamsarkar59@gmail.com>
2024-01-18 04:48:00 +0530

    Added Twitter icon and hover effect to quote component

--- src/components/qoute.svelte
+++ src/components/qoute.svelte
@@ -1,5 +1,6 @@
 <script>
 	import { onMount } from 'svelte';
+	import { IconBrandTwitter } from '@tabler/icons-svelte'
 
 	let joke = '';
 
@@ -20,7 +21,46 @@
 </script>
 
 <span
-	class="variant-glass-surface h-fit w-1/4 rounded-md p-5 left-2/4 bottom-4 absolute -translate-x-1/2"
+	class="variant-glass-surface h-fit w-1/4 rounded-md p-5 left-2/4 bottom-4 absolute -translate-x-1/2 hover"
 >
+	<span class="variant-glass-surface block absolute top-1/2 -translate-y-1/2 w-fit h-fit p-2 rounded-full hover-circle"><IconBrandTwitter/></span>
 	<p class="text-2xl text-center h-2/3">{joke}</p>
 </span>
+
+<style>
+	.hover-circle {
+		transition: opacity 0.3s ease;
+	}
+
+    .hover:hover .hover-circle{
+		opacity: 1;
+		animation: apperance 0.5s ease forwards;
+    }
+
+	.hover:not(:hover) .hover-circle{
+		opacity: 0;
+		animation: disapperance 5s ease;
+	}
+	@keyframes apperance {
+		0% {
+			transform: translate(0, -50%);
+		}
+		100% {
+			transform: translate(-180%, -50%);
+		}
+	}
+	@keyframes disapperance {
+		0% {
+			transform: translate(-180%, -50%);
+			opacity: 1;
+		}
+		90% {
+			transform: translate(-180%, -50%);
+			opacity: 1;
+		}
+		100% {
+			opacity: 0;
+			transform: translate(0, -50%);
+		}
+	}
+</style>
