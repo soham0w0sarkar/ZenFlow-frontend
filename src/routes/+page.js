e1e8683644d8d16e885a426ef11155af25b01e89
soham sarkar <sohamsarkar59@gmail.com>
2024-01-18 18:20:21 +0530

    putting correct api route

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,7 +1,7 @@
 import { isAuthenticated } from '../lib/store.js';
 
 export async function load({ fetch }) {
-	const res = await fetch(`http://localhost:9090/auth/status`);
+	const res = await fetch(`http://localhost:9090/api/v1/auth/status`);
 	const data = await res.json();
 
 	isAuthenticated.set(data.success);
