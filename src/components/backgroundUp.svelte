af2bebc93fb887808378d42bb22c6b28a801a3fa
soham sarkar <sohamsarkar59@gmail.com>
2024-01-20 23:57:26 +0530

    code format

--- src/components/backgroundUp.svelte
+++ src/components/backgroundUp.svelte
@@ -1,37 +1,13 @@
 <script>
 	import { onMount } from 'svelte';
 	import { IconPhotoScan } from '@tabler/icons-svelte';
-	import { FileDropzone } from '@skeletonlabs/skeleton';
-	import { IconFileUpload } from '@tabler/icons-svelte';
 	import Modal from './modal.svelte';
 
 	let showModal = false;
-	let file;
 
 	const handleClick = () => {
 		showModal = true;
 	};
-
-	const handleDrop = () => {
-		const formData = new FormData();
-		formData.append('file', file[0]);
-		console.log(file[0]);
-		try {
-			const res = fetch('http://localhost:9090/api/v1/background/setBackground', {
-				method: 'POST',
-				body: formData
-			});
-			console.log('Uploaded......');
-		} catch (error) {
-			console.log(error);
-		}
-	};
-
-	onMount(() => {
-		document.addEventListener('keydown', (e) => {
-			showModal = false;
-		});
-	});
 </script>
 
 <button
@@ -39,17 +15,5 @@
 	on:click={handleClick}
 >
 	<IconPhotoScan size={45} class="-z-10" />
-	<Modal {showModal}>
-		<FileDropzone
-			name="files"
-			bind:files={file}
-			class="h-full w-full variant-glass-surface text-xl text-white"
-			on:change={handleDrop}
-			accept="image/jpeg"
-		>
-			<svelte:fragment slot="lead"><IconFileUpload size={40} /></svelte:fragment>
-			<svelte:fragment slot="message"><b>Upload or Drag</b> file</svelte:fragment>
-			<svelte:fragment slot="meta"><b>Jpg</b> allowed</svelte:fragment>
-		</FileDropzone>
-	</Modal>
 </button>
+<Modal {showModal} />
--- src/components/modal.svelte
+++ src/components/modal.svelte
@@ -1,17 +1,54 @@
 <script>
+	import { FileDropzone } from '@skeletonlabs/skeleton';
+	import { IconFileUpload, IconX } from '@tabler/icons-svelte';
+
 	export let showModal;
 
 	let dialog;
+	let file;
+
 	$: if (dialog && showModal == true) {
 		dialog.showModal();
 	}
+
+	const handleDrop = () => {
+		const formData = new FormData();
+		formData.append('file', file[0]);
+		console.log(file[0]);
+		try {
+			const res = fetch('http://localhost:9090/api/v1/background/setBackground', {
+				method: 'POST',
+				body: formData
+			});
+			console.log('Uploaded......');
+		} catch (error) {
+			console.log(error);
+		}
+	};
 </script>
 
 <dialog
-	on:close={() => (showModal = false)}
+	bind:this={dialog}
 	class="variant-ghost-surface flex p-2 justify-center items-center rounded-md h-1/4 w-1/4 {showModal
 		? 'block'
 		: 'hidden'}"
 >
-	<slot />
+	<button
+		class="absolute top-2 right-2 variant-filled-primary"
+		on:click={() => {
+			dialog.close();
+			showModal = false;
+		}}><IconX size={15} /></button
+	>
+	<FileDropzone
+		name="files"
+		bind:files={file}
+		class="h-full w-full variant-glass-surface text-xl text-white"
+		on:change={handleDrop}
+		accept="image/jpeg"
+	>
+		<svelte:fragment slot="lead"><IconFileUpload size={40} /></svelte:fragment>
+		<svelte:fragment slot="message"><b>Upload or Drag</b> file</svelte:fragment>
+		<svelte:fragment slot="meta"><b>Jpg</b> allowed</svelte:fragment>
+	</FileDropzone>
 </dialog>
