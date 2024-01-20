eaf1474aacc325b7d18533521b52ddd8cad1fdf2
soham sarkar <sohamsarkar59@gmail.com>
2024-01-21 04:12:14 +0530

    Refactor hover-circle in qoute.svelte

--- src/components/qoute.svelte
+++ src/components/qoute.svelte
@@ -21,18 +21,19 @@
 </script>
 
 <span class="variant-glass-surface h-fit w-1/4 rounded-md p-5 left-2/4 bottom-4 absolute -translate-x-1/2 hover">
-	<span class="variant-glass-surface block absolute top-1/2 -translate-y-1/2 w-fit h-fit p-2 rounded-full hover-circle">
+	<button class="variant-glass-surface block absolute top-1/2 -translate-y-1/2 w-fit h-fit p-2 rounded-full hover-circle">
 		<IconBrandTwitter />
-	</span>
+	</button>
 	<p class="text-2xl text-center h-2/3">{joke}</p>
 </span>
 
 <style>
 	.hover-circle {
 		transition: opacity 0.3s ease;
+		pointer-events: none;
 	}
 
-	.hover:hover .hover-circle {
+	.hover-circle {
 		opacity: 1;
 		animation: apperance 0.5s ease forwards;
 	}
