1a1969a04d01620dec46aade2c4019a20dcb2495
soham sarkar <sohamsarkar59@gmail.com>
2024-01-24 04:46:35 +0530

    Refactor background handling in setting panel

--- src/components/pannels/setting.svelte
+++ src/components/pannels/setting.svelte
@@ -1,14 +1,79 @@
 <script>
-	import { backgroundUrls, backgroundUrl } from '../../lib/store.js';
+	const VITE_API_URL  = import.meta.env.VITE_API_URL;
+	import { backgrounds, backgroundUrl } from '../../lib/store.js';
+	import { IconCheck, IconTrash } from '@tabler/icons-svelte';
 
-	const handleCLick = (e) => {
-		$backgroundUrl = e.target.getAttribute('data-bg-image');
+    const handleDelete = async (e, bg) => {
+        e.stopPropagation(); 
+		backgrounds.update((bgs) => bgs.filter((tempBg) => tempBg.id !== bg.id));
+
+		if($backgroundUrl === bg.url) {
+			if($backgrounds[0]) {
+				$backgroundUrl = $backgrounds[0].url;
+			} else {
+				$backgroundUrl = '';
+				console.log('here');
+			}
+		}
+
+		try {
+			await deleteBackground(bg.id);
+		} catch (error) {
+			console.error(error);
+		}
 	};
+
+	const deleteBackground = async (id) => {
+		await fetch(`${VITE_API_URL}/background/deleteBackground/${id}`, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		});
+    };
+
+    const handleSet = (e, bg) => {
+        e.stopPropagation();
+        $backgroundUrl = bg;
+    };
 </script>
 
 <div class="h-full w-full p-2 flex flex-col items-center">
 	<div class="text-2xl text-left w-full">Backgrounds</div>
-	{#each $backgroundUrls as bgUrl}
-		<button class="w-2/3 h-1/6 rounded-md m-2 shadow-xl bg-cover" style="background-image: url('{bgUrl}');" data-bg-image={bgUrl} on:click={handleCLick}></button>
+	{#each $backgrounds as bg}
+		<div class="w-2/3 h-1/6 rounded-md m-2 shadow-xl bg-cover relative image" style="background-image: url('{bg.url}');">
+			<div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 buttons">
+				<button class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleSet(e, bg.url)}><IconCheck/></button>
+				<button class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleDelete(e, bg)}><IconTrash/></button>
+			</div>
+		</div>
 	{/each}
 </div>
+
+<style>
+	.buttons {
+		display: none;
+	}
+	.image:hover .buttons {
+		display: flex;
+	}
+
+	.buttons {
+		transition: opacity 0.3s ease;
+		animation: appear 0.3s ease;
+	}
+
+	button {
+		transition: all 0.2s ease;
+	}
+
+	@keyframes appear {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
+</style>
