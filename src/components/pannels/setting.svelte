ac527866ca9485aa9d2aea76eed4af3a04cff4f6
soham sarkar <sohamsarkar59@gmail.com>
2024-01-25 04:57:38 +0530

    code formatting

--- src/components/pannels/setting.svelte
+++ src/components/pannels/setting.svelte
@@ -1,14 +1,14 @@
 <script>
-	const VITE_API_URL  = import.meta.env.VITE_API_URL;
+	const VITE_API_URL = import.meta.env.VITE_API_URL;
 	import { backgrounds, backgroundUrl } from '../../lib/store.js';
 	import { IconCheck, IconTrash } from '@tabler/icons-svelte';
 
-    const handleDelete = async (e, bg) => {
-        e.stopPropagation(); 
+	const handleDelete = async (e, bg) => {
+		e.stopPropagation();
 		backgrounds.update((bgs) => bgs.filter((tempBg) => tempBg.id !== bg.id));
 
-		if($backgroundUrl === bg.url) {
-			if($backgrounds[0]) {
+		if ($backgroundUrl === bg.url) {
+			if ($backgrounds[0]) {
 				$backgroundUrl = $backgrounds[0].url;
 			} else {
 				$backgroundUrl = '';
@@ -30,12 +30,12 @@
 				'Content-Type': 'application/json'
 			}
 		});
-    };
+	};
 
-    const handleSet = (e, bg) => {
-        e.stopPropagation();
-        $backgroundUrl = bg;
-    };
+	const handleSet = (e, bg) => {
+		e.stopPropagation();
+		$backgroundUrl = bg;
+	};
 </script>
 
 <div class="h-full w-full p-2 flex flex-col items-center">
@@ -43,8 +43,8 @@
 	{#each $backgrounds as bg}
 		<div class="w-2/3 h-1/6 rounded-md m-2 shadow-xl bg-cover relative image" style="background-image: url('{bg.url}');">
 			<div class="absolute inset-0 flex items-center justify-center bg-black bg-opacity-50 buttons">
-				<button aria-label="set-background" class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleSet(e, bg.url)}><IconCheck/></button>
-				<button aria-label="delete" class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleDelete(e, bg)}><IconTrash/></button>
+				<button aria-label="set-background" class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleSet(e, bg.url)}><IconCheck /></button>
+				<button aria-label="delete" class="m-2 p-2 rounded hover:shadow-glow" on:click={(e) => handleDelete(e, bg)}><IconTrash /></button>
 			</div>
 		</div>
 	{/each}
@@ -73,5 +73,4 @@
 			opacity: 1;
 		}
 	}
-
 </style>
