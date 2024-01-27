4a1c50449616f6a59f2f4eea0e3d1d0af7baeefa
soham sarkar <sohamsarkar59@gmail.com>
2024-01-27 12:07:03 +0530

    added summary section to calendar panel

--- src/components/pannels/calendar.svelte
+++ src/components/pannels/calendar.svelte
@@ -16,3 +16,13 @@
         }
     })
 </script>
+<div class="h-full w-full">
+    <div class="h-fit w-full variant-ghost-surface p-2 flex flex-col items-center">
+        <div class="w-5/6 h-fit p-2 text-2xl font-bold variant-glass-success rounded-md flex justify-between">
+            <span class="h-fit w-fit">Summary</span>
+            <button>
+                <IconChevronDown/>
+            </button>
+        </div>
+    </div>
+</div>
