b05ecddfc17c0c35484758591ee2e1a9d3e43d78
soham sarkar <sohamsarkar59@gmail.com>
2024-02-03 22:01:06 +0530

    Refactor mail panel component and add API call for fetching all mails

--- src/components/pannels/mail.svelte
+++ src/components/pannels/mail.svelte
@@ -1,11 +1,25 @@
 <script>
-    import { IconUserCircle } from "@tabler/icons-svelte";
+	import { IconUserCircle } from '@tabler/icons-svelte';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const res = await fetch(`/api/widgets/allMails/`, {
+			method: 'GET',
+			credentials: 'include'
+		});
+
+		const data = await res.json();
+
+		if (data.status) {
+			console.log(data);
+		}
+	});
 </script>
 
 <div class="h-full w-full p-2">
-    <h1 class="w-full font-extrabold">Today's Mail</h1>
-    <span class="h-fit w-full p-2 text-2xl rounded-md mt-2 variant-glass-surface flex items-center">
-        <span class="h-full w-fit"><IconUserCircle size={38}/></span>
-        <span class="w-fit h-full ml-2">Mail Content Here</span>
-    </span>
+	<h1 class="w-full font-extrabold">Today's Mail</h1>
+	<span class="h-fit w-full p-2 text-2xl rounded-md mt-2 variant-glass-surface flex items-center">
+		<span class="h-full w-fit"><IconUserCircle size={38} /></span>
+		<span class="w-fit h-full ml-2">Mail Content Here</span>
+	</span>
 </div>
