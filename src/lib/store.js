46532af2e1190edeb1b28665dd2a8e4e26a4566e
soham sarkar <sohamsarkar59@gmail.com>
2024-02-05 03:52:39 +0530

    updated backgroundUrl in store.js

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,5 +2,5 @@ import { writable } from 'svelte/store';
 
 export const isAuthenticated = writable(false);
 export const backgrounds = writable([{}]);
-export const backgroundUrl = writable('');
+export const backgroundUrl = writable('https://res.cloudinary.com/dwcquwmpw/image/upload/v1707084584/wallpaperflare.com_wallpaper_1_nrbh4c.jpg');
 export const joke = writable('');
