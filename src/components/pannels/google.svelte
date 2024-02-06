0d70e196796f655f938258b79d454b15e7baf7fb
soham sarkar <sohamsarkar59@gmail.com>
2024-02-06 20:00:14 +0530

    updated Google icon in Google panel

--- src/components/pannels/google.svelte
+++ src/components/pannels/google.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { IconBrandGoogleFilled } from "@tabler/icons-svelte";
+	import { IconBrandGoogle } from "@tabler/icons-svelte";
 
     let focused = false;
     
@@ -16,7 +16,7 @@
 <div class="h-full w-full p-2">
     <div class="h-fit w-full flex justify-center transition-all pl-2 rounded-full items-center {focused ? 'variant-ghost-surface':'bg-transparent'} duration-200">
         {#if focused}
-            <IconBrandGoogleFilled />
+            <IconBrandGoogle />
         {/if}
         <input type="text" name="google" on:focus={focus} on:blur={blur}  id="google" class="w-11/12 {focused ? 'bg-transparent':'variant-ghost-surface'} text-xl rounded-full focus:border-transparent  focus:ring-0" placeholder="{focused ? '' : 'Search'}"/>
     </div>
