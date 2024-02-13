7b7ae13e313ae7e5c557c90d4b1654392d520912
soham sarkar <sohamsarkar59@gmail.com>
2024-02-13 17:25:27 +0530

    added currentCard to keep track of current card in +page.svelte and weather to store weather data retrieved form backend

--- src/lib/store.js
+++ src/lib/store.js
@@ -4,3 +4,5 @@ export const isAuthenticated = writable(false);
 export const backgrounds = writable([{}]);
 export const backgroundUrl = writable('https://res.cloudinary.com/dwcquwmpw/image/upload/v1707084584/wallpaperflare.com_wallpaper_1_nrbh4c.jpg');
 export const joke = writable('');
+export const currentCard = writable(-1);
+export const weather = writable({});
