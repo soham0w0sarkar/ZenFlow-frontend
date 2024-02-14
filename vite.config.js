26761f7064d0daf8bb492ab52ea9f7bd7b65c316
soham sarkar <sohamsarkar59@gmail.com>
2024-02-15 01:22:23 +0530

    updated API target URL in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:9090/',
+				target: 'https://zenflow-77lc.onrender.com/',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '/api/v1')
 			}
