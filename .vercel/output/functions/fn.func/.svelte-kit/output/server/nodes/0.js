774b91ce86443d36b20b8046186b0fb0b08ab31c
soham sarkar <sohamsarkar59@gmail.com>
2024-02-18 00:56:03 +0530

    deleted unused files and configurations

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.DWgu611C.js","_app/immutable/chunks/scheduler.BNBspLx6.js","_app/immutable/chunks/index.Vt5M9wIh.js","_app/immutable/chunks/index.6Dl2lb1K.js","_app/immutable/chunks/Icon.pM8mz-m1.js"];
-export const stylesheets = ["_app/immutable/assets/0.BzcFa2zl.css"];
-export const fonts = [];
--- .vercel/output/static/_app/immutable/entry/start.6_BMxYlE.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.CK4fRsCe.js";export{t as start};
