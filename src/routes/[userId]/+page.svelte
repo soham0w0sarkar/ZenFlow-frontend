cb8086a2a2bdf46ffc8ecaf20627729073b10248
soham sarkar <sohamsarkar59@gmail.com>
2024-02-19 21:46:24 +0530

    added +page.svelte component for user-specific pages

---
+++ src/routes/[userId]/+page.svelte
@@ -0,0 +1,32 @@
+<script>
+	import { isAuthenticated } from './../../lib/store.js';
+	import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+    import { onMount } from 'svelte';
+
+    const userId = $page.params.userId;
+
+    let mounted = false;
+
+    const setCredentials = async (userId) => {
+		const res = await fetch(`/api/auth/setCredentials/${encodeURIComponent(userId)}`, {
+			method: 'GET',
+			credentials: 'include',
+		});
+		const data = await res.json();
+
+		if(data.success) {
+            $isAuthenticated = true;
+			goto('../')
+		}
+	};
+
+    $: userId && mounted && setCredentials(userId);
+
+    onMount(() => {
+        mounted = true;
+    });
+	
+</script>
+
+<div class="h-full w-full flex justify-center items-center text-5xl">PROCESSING ......</div>
