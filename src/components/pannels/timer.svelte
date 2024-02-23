d0079155f2aee319cb6c6cca0ba18799408fea7c
soham sarkar <sohamsarkar59@gmail.com>
2024-02-23 19:16:42 +0530

    fixed timer

--- src/components/pannels/timer.svelte
+++ src/components/pannels/timer.svelte
@@ -1,120 +1,134 @@
 <script>
-	import { onMount, onDestroy } from 'svelte';
-	import { IconPlayerPlayFilled, IconPlayerStopFilled } from '@tabler/icons-svelte';
+    import { onMount, onDestroy } from 'svelte';
+    import { IconPlayerPlayFilled, IconPlayerStopFilled } from '@tabler/icons-svelte';
 
-	let count = 0;
-	let rotation = 0;
-	let seconds = 0;
-	let clicked = false;
-	let play = false;
+    let rotation = 0;
+	let min = 0;
+    let seconds = 0;
+    let clicked = false;
+    let play = false;
+    let intervalId;
+    let startTime;
 
-	$: seconds = count * 60;
-	$: rotation = (seconds / 3600) * 360;
-	$: if (play) handleTimer(true);
+	$: min = Math.ceil(seconds / 60);
+    $: rotation = (seconds / 3600) * 360;
+    $: if (play) handleTimer(true);
 
-	const keydown = (e) => {
-		if (e.key === 'ArrowUp') {
-			count += 5;
-		} else if (e.key === 'ArrowDown') {
-			if (count > 0) count -= 1;
-		}
-	};
+    const keydown = (e) => {
+        if (e.key === 'ArrowUp') {
+            seconds += 300; // Increase by 5 minutes in seconds
+        } else if (e.key === 'ArrowDown') {
+            if (seconds > 0) seconds -= 60; // Decrease by 1 minute in seconds
+        }
+    };
 
-	const timer = () => {
-		if (seconds > 0) {
-			seconds -= 1;
-			if (seconds % 60 === 0) {
-				count -= 1;
-			}
-		} else {
-			handleTimer(false);
-		}
-	};
+    const timer = () => {
+        if (seconds > 0) {
+            seconds -= 1;
+        } else {
+            handleTimer(false);
+        }
+    };
 
-	const handleTimer = (start) => {
-		if (start) {
-			setInterval(timer, 1000);
-			play = true;
-		} else {
-			clearInterval(timer);
-			count = 0;
-			play = false;
-		}
-	};
+    const handleTimer = (start) => {
+        if (intervalId) {
+            clearInterval(intervalId);
+            intervalId = null;
+        }
 
-	onMount(() => {
-		count = localStorage.getItem('count') || 0;
-		seconds = localStorage.getItem('seconds', seconds);
-		play = localStorage.getItem('play', play);
+        if (start) {
+            startTime = Date.now();
+            intervalId = setInterval(timer, 1000);
+            play = true;
+        } else {
+            seconds = 0;
+            play = false;
+        }
+    };
 
-		document.querySelector('.point').addEventListener('click', () => {
-			if (!clicked) {
-				document.addEventListener('keydown', keydown);
-			} else {
-				document.removeEventListener('keydown', keydown);
-			}
-			clicked = !clicked;
-		});
-	});
+    onMount(() => {
+        const storedSeconds = parseInt(localStorage.getItem('seconds')) || 0;
+        const storedStartTime = parseInt(localStorage.getItem('startTime'));
+        play = localStorage.getItem('play') === 'true';
 
-	onDestroy(() => {
-		localStorage.setItem('count', count);
-		localStorage.setItem('seconds', seconds);
-		localStorage.setItem('play', play);
-	});
+        if (play && storedStartTime) {
+            const elapsedSeconds = Math.floor((Date.now() - storedStartTime) / 1000);
+            seconds = Math.max(storedSeconds - elapsedSeconds, 0);
+        } else {
+            seconds = storedSeconds;
+        }
+
+        document.querySelector('.point').addEventListener('click', () => {
+            if (!clicked) {
+                document.addEventListener('keydown', keydown);
+            } else {
+                document.removeEventListener('keydown', keydown);
+            }
+            clicked = !clicked;
+        });
+    });
+
+    onDestroy(() => {
+        localStorage.setItem('seconds', seconds.toString());
+        localStorage.setItem('play', play.toString());
+        localStorage.setItem('startTime', startTime.toString());
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+    });
 </script>
 
 <div class="h-full w-full">
-	<div class="w-full h-1/4 flex flex-col justify-evenly items-center">
-		<span class="text-8xl">{count}</span>
-		<span class="text-4xl">MIN</span>
-	</div>
-	<div class="h-3/4 w-full flex flex-col items-center">
-		<button class="w-0 h-0 m-2 point"></button>
-		<div
-			class="flex justify-center items-center h-60 w-60 variant-glass-surface rounded-full relative p-4 transition-all"
-			style="transform: rotate({rotation}deg);"
-		>
-			<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute top-1" style="transform: rotate(-{rotation}deg);">
-				{Math.floor(count / 60) * 60}
-			</span>
-			<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute left-1" style="transform: rotate(-{rotation}deg);">
-				{Math.floor(count / 60) * 60 + 15}
-			</span>
-			<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute bottom-1" style="transform: rotate(-{rotation}deg);">
-				{Math.floor(count / 60) * 60 + 30}
-			</span>
-			<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute right-1" style="transform: rotate(-{rotation}deg);">
-				{Math.floor(count / 60) * 60 + 45}
-			</span>
-			{#if count > 0}
-				<button
-					class="variant-filled-surface w-1/4 h-1/4 rounded-full p-2"
-					on:click={() => {
-						handleTimer(!play);
-					}}
-					style="transform: rotate(-{rotation}deg);"
-				>
-					{#if play}
-						<IconPlayerStopFilled size={38} />
-					{:else}
-						<IconPlayerPlayFilled size={38} />
-					{/if}
-				</button>
-			{/if}
-		</div>
-	</div>
+    <div class="w-full h-1/4 flex flex-col justify-evenly items-center">
+        <span class="text-8xl">{min}</span>
+        <span class="text-4xl">MIN</span>
+    </div>
+    <div class="h-3/4 w-full flex flex-col items-center">
+        <button class="w-0 h-0 m-2 point"></button>
+        <div
+            class="flex justify-center items-center h-60 w-60 variant-glass-surface rounded-full relative p-4 transition-all"
+            style="transform: rotate({rotation}deg);"
+        >
+				<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute top-1" style="transform: rotate(-{rotation}deg);">
+					{Math.floor(min / 60) * 60}
+				</span>
+				<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute left-1" style="transform: rotate(-{rotation}deg);">
+					{Math.floor(min / 60) * 60 + 15}
+				</span>
+				<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute bottom-1" style="transform: rotate(-{rotation}deg);">
+					{Math.floor(min / 60) * 60 + 30}
+				</span>
+				<span class="w-6 h-6 m-1 text-center text-lg font-bold absolute right-1" style="transform: rotate(-{rotation}deg);">
+					{Math.floor(min / 60) * 60 + 45}
+				</span>
+            {#if seconds > 0}
+                <button
+                    class="variant-filled-surface w-1/4 h-1/4 rounded-full p-2"
+                    on:click={() => {
+                        handleTimer(!play);
+                    }}
+                    style="transform: rotate(-{rotation}deg);"
+                >
+                    {#if play}
+                        <IconPlayerStopFilled size={38} />
+                    {:else}
+                        <IconPlayerPlayFilled size={38} />
+                    {/if}
+                </button>
+            {/if}
+        </div>
+    </div>
 </div>
 
 <style>
-	.point {
-		border-left: 15px solid transparent;
-		border-right: 15px solid transparent;
-		border-top: 30px solid #f00;
-	}
+    .point {
+        border-left: 15px solid transparent;
+        border-right: 15px solid transparent;
+        border-top: 30px solid #f00;
+    }
 
-	span,
-	button {
-		transition: all 100ms ease-in-out;
-	}
+    span,
+    button {
+        transition: all 100ms ease-in-out;
+    }
 </style>
