45f047e997d6a6531b7cc24a07f30bab3cc26c62
soham sarkar <sohamsarkar59@gmail.com>
2024-02-26 14:44:46 +0530

    'changed to webp

--- src/lib/store.js
+++ src/lib/store.js
@@ -2,7 +2,7 @@ import { writable } from 'svelte/store';
 
 export const isAuthenticated = writable(false);
 export const backgrounds = writable([{}]);
-export const backgroundUrl = writable('https://res.cloudinary.com/dwcquwmpw/image/upload/v1707084584/wallpaperflare.com_wallpaper_1_nrbh4c.jpg');
+export const backgroundUrl = writable('https://res.cloudinary.com/dwcquwmpw/image/upload/v1708938820/wallpaperflare.com_wallpaper_1_bt0cpf.webp');
 export const joke = writable('');
 export const currentCard = writable(-1);
 export const weather = writable({});
